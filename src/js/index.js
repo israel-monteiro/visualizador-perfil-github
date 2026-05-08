7aa23663a3a981d8c4f9e51453c045aa47f1c1cb
Israel Monteiro <israelmonteiro.dev@gmail.com>
2026-05-08 15:13:05 -0300

    feat: add repository search on Enter key press

--- src/js/index.js
+++ src/js/index.js
@@ -5,27 +5,33 @@ const inputSearch = document.getElementById("input-search");
 const btnSearch = document.getElementById("btn-search");
 const profileResults = document.querySelector(".profile-results");
 
-btnSearch.addEventListener("click", async () => {
-  const userName = inputSearch.value.trim();
+async function getUserProfile(p) {
+    const userName = inputSearch.value.trim();
 
-  if (userName) {
-    try {
-      profileResults.innerHTML = '<div class="loading">Carregando...</div>';
+    if (userName) {
+        try {
+            profileResults.innerHTML = '<div class="loading">Carregando...</div>';
 
-      const userData = await fetchGitHubUser(userName);
-      const userRepos = await fetchGitHubUserRepos(userName);
-      console.log(userData);
-      console.log(userRepos);
+            const userData = await fetchGitHubUser(userName);
+            const userRepos = await fetchGitHubUserRepos(userName);
+            console.log(userData);
+            console.log(userRepos);
 
-      renderProfile(userData, userRepos, profileResults);
-
-    } catch (error) {
-      profileResults.innerHTML = "";
-      console.log("Erro ao buscar o perfil do usuário: ", error);
-      alert("Ocorreu um erro ao buscar o usuário. Por favor, tente novamente mais tarde.");
+            renderProfile(userData, userRepos, profileResults);
+        } catch (error) {
+            profileResults.innerHTML = "";
+            console.log("Erro ao buscar o perfil do usuário: ", error);
+            alert("Ocorreu um erro ao buscar o usuário. Por favor, tente novamente mais tarde.");
+        }
+    } else {
+        alert("Por favor, digite o nome de usuário do GitHub.");
     }
-  } else {
-    alert("Por favor, digite o nome de usuário do GitHub.");
-  }
-});
+}
 
+btnSearch.addEventListener("click", getUserProfile);
+
+inputSearch.addEventListener("keyup", (e) => {
+    if (e.key === "Enter") {
+        btnSearch.click();
+    }
+});
